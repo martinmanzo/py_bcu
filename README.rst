3bc21574554867491c243dd3cc3be77108c02d39
Martin Manzo <manzo.martin@hotmail.com>
2019-03-01 14:46:19 -0300

    added install requires

--- README.rst
+++ README.rst
@@ -1,1 +1,7 @@
+PyPi: https://pypi.org/project/py-bcu/
+
+pip install py-bcu
+
 Inspirado en: https://github.com/biller/bcu
+
+https://blog.jetbrains.com/pycharm/2017/05/how-to-publish-your-package-on-pypi/
--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup
 
 setup(
     name='py_bcu',
-    version='0.1.0',
+    version='0.1.1',
     packages=['py_bcu'],
-    package_dir={'': 'src'},
+    package_dir={'': '.'},
     url='https://github.com/martinmanzo/py_bcu/',
     license='MIT',
     author='Martin Manzo',
     author_email='',
-    description='Interact with the BCU\'s (Banco Central del Uruguay) webservices.'
+    description='Interact with the BCU\'s (Banco Central del Uruguay) webservices.',
+    install_requires=[
+        'zeep==3.2.0'
+    ]
 )
